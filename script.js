4f07f75953be94a5d1547a07d68c67a9903d071b
Jeff Lebowski <anentitymusic@gmail.com>
2021-04-14 17:55:49 -0400

    created character variable universe

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var abcUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var abcUpperArr = abcUpper.split("");
+var abcLower = "abcdefghijklmnopqrstuvwxyz";
+var abcLowerArr = abcLower.split("");
+var num = "0123456789";
+var numArr = num.split("");
+var sym = "!#$%&\()*+,-./:;<=>?@^[\\]^_`{|}~";
+var symArr = sym.split("");
+
+alert("Welcome to Stephen's Password Generator.  Please click 'ok' to begin!");
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
